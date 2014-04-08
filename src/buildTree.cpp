ca288dc8f3cd990d99bf23bba5430bfecc803bce
chiggum <codechiggum@gmail.com>
2014-04-08 20:40:34 +0530

    Blueprint made

--- src/buildTree.cpp
+++ src/buildTree.cpp
@@ -35,5 +35,16 @@ void buildTree(struct Node* node,
 			   int numPixels,
 			   int numSegments)
 {
-	// Need to be implemented
+	/* Blueprint:
+		Initialize the leaves of the tree as the pixels of the image i.e. nodes passed in the argument.
+		Initialize an adptable max priority queue with the key as the pairs of the leaves/pixels and value as the Cost inverse of the pair of nodes calvulated using compCostInv
+		Now extract max from the priority queue => check the key and make the corresponding pixels or leaves or nodes as the children of the union
+		Say x1 U x2 is the key of the max entry => replace all priority queue entries with x1 as one of the entry of the key with x1 U x2 as the entry and hence change the attributes of the entry
+		Also remove all those entries with x2 as one of the key of the entry and hence correct the configuration of the max priority queue
+		Now repeat again form step 3 until  we get the desired number of segments
+	*/
+	/* 
+	We should return the roots/Nodes at top level of the tree 
+	// Hence, that has to be rectified in the code
+
 }
